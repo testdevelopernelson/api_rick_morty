86865e3c01db2338f9fa92df38fb429c4103b17e
Nelson Montoya <test.developer.nelson@gmail.com>
2021-04-24 15:50:51 -0500

    conection DB

--- resources/views/default/layouts/master.blade.php
+++ resources/views/default/layouts/master.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=0" />
-    <title>Prueba El lab</title>     
+    <title>Prueba El lab</title>  
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">    
     
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="base-url" content="{{ url('/') }}" />
